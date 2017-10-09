4dc0a41e297790e79ef8e53656ab10edac04b454
groxni <nishukai@hotmail.com>
2017-10-09 21:15:37 +0800

    Proxy site supported

--- asmSpider/spiders/__init__.py
+++ asmSpider/spiders/__init__.py
@@ -20,22 +20,20 @@ class MangaLinkSpider(scrapy.Spider):
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
 
-
-class VCBSpider(scrapy.Spider):
-    name = "vcb"
-    baseLink = 'https://vcb-s.com/' 
+class proxylistSpider(scrapy.Spider):
+    name = "proxylist"
     start_urls = [
-        baseLink
+        "https://free-proxy-list.net/",
     ]
-
-    def parse(self, response, baseLink=baseLink):
-        for image in response.css('div.preview_item'):
+    def parse(self, response):
+        for li in response.css('tbody tr'):
             yield {
-                'title': image.css('div.caption a::text').extract_first(),
-                'link': baseLink+image.css('div.image a::attr(href)').extract_first(),
-                'thumbnail': 'https:'+image.css('div.image img::attr(src)').extract_first(),
+                'Ip': li.css('td::text')[0].extract(),
+                'Port': li.css('td::text')[1].extract(),
+                'Code': li.css('td::text')[2].extract(),
+                'Country': li.css('td::text')[3].extract(),
+                'Anonymity': li.css('td::text')[4].extract(),
+                'Google': li.css('td::text')[5].extract(),
+                'Https': li.css('td::text')[6].extract(),
+                'Last Checked': li.css('td::text')[7].extract(),
             }
-
-        next_page = baseLink+'/pag/'+str(int(response.css('span.current::text').extract_first())+1)+'/'
-        if next_page is not None:
-            yield response.follow(next_page, callback=self.parse)
